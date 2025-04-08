7a7bbe1c9a961aea444d383fe55131f46c006eed
Gabriel Dias <gabesarcal215@gmail.com>
2025-04-08 19:41:41 -0300

    Primeiro Commit PlayFut

---
+++ PlayFut/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayFut
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PlayFut/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PlayFut
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
