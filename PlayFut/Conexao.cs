a039153101d1aa050c13ee8fa934f1c31e32f67f
guerreiro05 <viniarsenios8@gmail.com>
2025-05-08 20:11:10 -0300

    att

--- PlayFut/Conexao.cs
+++ PlayFut/Conexao.cs
@@ -1,36 +1,56 @@
-using PlayFut;
 using MySql.Data.MySqlClient;
+using System;
 using System.Data;
 
 namespace PlayFut
 {
     class Conexao
     {
-        string dadosConexao = "server=localhost;user=root;password=;database=playfut;port=3306;";
-
+        // Corrigido: charset adicionado para suporte a acentuação e símbolos especiais
+        private readonly string dadosConexao = "server=localhost;user=root;password=;database=playfut;port=3306;charset=utf8mb4;";
 
         public int ExecutaComando(string query)
         {
-            using (var conexao = new MySqlConnection(dadosConexao))
+            try
             {
-                conexao.Open();
-                using var comando = new MySqlCommand(query, conexao);
-                return comando.ExecuteNonQuery();
-            } // A conexão fecha automaticamente pelo using
+                using (var conexao = new MySqlConnection(dadosConexao))
+                {
+                    conexao.Open();
+                    using (var comando = new MySqlCommand(query, conexao))
+                    {
+                        return comando.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Erro ao executar comando no banco de dados: " + ex.Message, ex);
+            }
         }
 
         public DataTable ExecutaSelect(string query)
         {
             var dt = new DataTable();
-            using (var conexao = new MySqlConnection(dadosConexao))
+            try
             {
-                conexao.Open();
-                using var comando = new MySqlCommand(query, conexao);
-                using var adapter = new MySqlDataAdapter(comando);
-                adapter.Fill(dt);
+                using (var conexao = new MySqlConnection(dadosConexao))
+                {
+                    conexao.Open();
+                    using (var comando = new MySqlCommand(query, conexao))
+                    {
+                        using (var adapter = new MySqlDataAdapter(comando))
+                        {
+                            adapter.Fill(dt);
+                        }
+                    }
+                }
             }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Erro ao executar SELECT no banco de dados: " + ex.Message, ex);
+            }
+
             return dt;
         }
-
     }
 }
